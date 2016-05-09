956c02cd7f7acac384ffb0419e608cbeff7786d7
root <root@Salt_Master.(none)>
2016-05-09 14:57:17 +0800

    优化主机信息获取

--- salts/views.py
+++ salts/views.py
@@ -25,16 +25,19 @@ def refresh_host_info(request):
     salt=SaltByLocalApi('/etc/salt/master')
     host_info_dict=salt.get_host_info()
     for host,info_list in host_info_dict.iteritems():
-	if HostInfoModel.objects.filter(hostname=info_list[0].strip('')).count() >0:
+	if HostInfoModel.objects.filter(hostname=host.strip('')).count() >0:
 	    continue
-	if HostInfoModel.objects.filter(hostname=info_list[0].strip('')).count()==0:
+	if HostInfoModel.objects.filter(hostname=host.strip('')).count()==0:
 	    if info_list[5] != '' and info_list[6] !='' and info_list[7] != '':
-	        host_info=HostInfoModel(hostname=info_list[0],ipaddress=info_list[1],cpuinfo=info_list[3],meminfo=info_list[4],group=info_list[5],osinfo=info_list[2],area=info_list[6],usage=info_list[7])
+	        host_info=HostInfoModel(hostname=host,ipaddress=info_list[1],cpuinfo=info_list[3],meminfo=info_list[4],group=info_list[5],osinfo=info_list[2],area=info_list[6],usage=info_list[7])
 	        try:
 		    host_info.save()
 	        except Exception as e:
-		    print e
-    hosts=HostInfoModel.objects.all().order_by('group')
+		    return 500
+    all_host=HostInfoModel.objects.all()
+    for host in all_host:
+	if host.hostname not in host_info_dict:
+	    host.delete()
     return  HttpResponseRedirect(reverse('salts:host_info'))
     
 
