b823d1b6bf398b73c47ae5c1e9d6203972c7dce0
ho_o <hjg@wikiki.cn>
2017-04-25 13:59:12 +0800

    add change user function

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from views import *
 urlpatterns=[
 	url(r'^userList/$',list_all_users,name='userList'),
 	url(r'^userAdd/$',add_user,name='userAdd'),
+	url(r'^changeUser/$',change_user_api,name='changeUser'),
 	]
--- users/views.py
+++ users/views.py
@@ -11,8 +11,14 @@ import json
 def list_all_users(request):
     users=User.objects.all()
     for u in users:
-	u.last_login=datetime.strftime(u.last_login,'%Y-%m-%d %H:%M:%S')
-	u.date_joined=datetime.strftime(u.date_joined,'%Y-%m-%d %H:%M:%S')
+	if u.last_login:
+	    u.last_login=datetime.strftime(u.last_login,'%Y-%m-%d %H:%M:%S')
+	else:
+	    u.last_login=''
+	if u.date_joined:
+	    u.date_joined=datetime.strftime(u.date_joined,'%Y-%m-%d %H:%M:%S')
+	else:
+	    u.date_joined=''
     return render_to_response('user_list.html',RequestContext(request,{'users':users}))
 
 @login_required(login_url='/')
@@ -34,3 +40,44 @@ def add_user(request):
 	    return HttpResponse(json.dumps({'code':400,'info':'add user failed'}))
 	return HttpResponse(json.dumps({'code':200,'info':'add user success'}))
     return HttpResponse(json.dumps({'code':400,'info':'error http method'}))
+
+
+
+
+@login_required(login_url='/')
+def change_user_api(request):
+    if request.method=='POST':
+	if not request.user.is_superuser:
+	    return HttpResponse(json.dumps({'code':400,'info':'No Permission !'}))
+	userid=request.POST.get('userid','').replace(' ','')
+	opt=request.POST.get('opt','').replace(' ','')
+	if not userid or not opt:
+	    return HttpResponse(json.dumps({'code':400,'info':'Error Params !'}))
+	u=User.objects.get(id=userid)
+        if not u:
+	    return HttpResponse(json.dumps({'code':400,'info':'User do not exist !'}))
+	print dir(u)
+	if opt == 'delete':
+	    try:
+		u.delete()
+	    except Exception as e:
+	        return HttpResponse(json.dumps({'code':400,'info':'Delete Failed'}))
+	elif opt == 'active':
+	    if not u.is_active:
+		u.is_active=1
+		try:
+		    u.save()
+		except Exception as e:
+	            return HttpResponse(json.dumps({'code':400,'info':'Active Failed'}))
+	elif opt == 'freeze':
+	    if u.is_active:
+		u.is_active=0
+		try:
+		    u.save()
+		except Exception as e:
+	            return HttpResponse(json.dumps({'code':400,'info':'Freeze Failed'}))
+        else:
+	    return HttpResponse(json.dumps({'code':400,'info':'Error Params'}))
+	return HttpResponse(json.dumps({'code':200,'info':'Change User Status Success'}))
+    else:
+	return HttpResponse(json.dumps({'code':400,'info':'Error Http Method'}))
