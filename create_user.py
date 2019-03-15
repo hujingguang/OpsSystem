6c42a6476cf66f122d4b5b0567fb02a9fe846dd8
hjg <hujingguangsa@gmail.com>
2019-03-15 14:57:18 +0800

    bugfix

---
+++ create_user.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import User
+def create_superuser():
+    user = User.objects.create_superuser("admin",'admin@gmail.com','helloworld')
+    user.save()
+create_superuser()
