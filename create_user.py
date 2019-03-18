c82afad45a4e3e149223082f86a71420b2d7530d
hjg <hujingguangsa@gmail.com>
2019-03-18 10:21:49 +0800

    modify

--- create_user.py
+++ create_user.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
+
 def create_superuser():
     user = User.objects.create_superuser("admin",'admin@gmail.com','helloworld')
     user.save()
+
 create_superuser()
